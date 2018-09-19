669f9ebe7b7e21ea9fb8bd56ba887a69773002ba
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-19 16:09:12 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -250,7 +250,7 @@ public function ApproveCenterEng($replyToken = null,$LineID)
 public function TimeAttendance($replyToken = null, $LineID)
 {
     $actions = array(
-        New UriTemplateActionBuilder("ลงเวลาเข้างาน", "https://".$Link."/LineService/TimeStamp/TimeStampInfo/".$LineID),
+        New UriTemplateActionBuilder("ลงเวลาเข้างาน", "https://lineservice.prosofthcm.com/LineService/TimeStamp/TimeStampInfo/".$LineID),
         New UriTemplateActionBuilder("ข้อมูลเวลาทำงาน", "https://lineservice.prosofthcm.com/LineService/WorkTime/WorkTimeInfo/".$LineID),
         New MessageTemplateActionBuilder("สิทธิ์การลาและวันลาคงเหลือ", "สิทธิ์การลาและวันลาคงเหลือ"),
         New UriTemplateActionBuilder("ข้อมูลการขอลา", "https://lineservice.prosofthcm.com/LineService/LeaveRequest/LeaveRequestList/".$LineID)
