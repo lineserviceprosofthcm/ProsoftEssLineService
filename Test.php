e73e25b9959d050f8462a75e7d9663123c4bd2d9
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-19 15:18:30 +0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,22 +1,7 @@
-
-
-
 <HTML>
 <HEAD><TITLE> My Homepage </TITLE></HEAD>
 <BODY BGCOLOR=#FFFFFF>
-     <?php
-        public $objOpen         = null;
-        public $file            = null;
-        public $LineAPIURL      = null;
-
-   
-    
-        $objOpen = opendir("URL");
-        $file = readdir($objOpen);
-        while (($file = readdir($objOpen)) !== false){ $LineAPIURL = $file; }
-
-        echo $LineAPIURL;
-    ?>
+<?php echo "Hello World"; ?>
   
 </BODY>
 </HTML>
