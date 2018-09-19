29de242ac9af14cb71d3532647427def3a3be334
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-19 17:33:10 +0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -2,11 +2,16 @@
 <HEAD><TITLE> My Homepage </TITLE></HEAD>
 <BODY BGCOLOR=#FFFFFF>
 <?php 
-  echo "Hello World"; 
-  //$files = glob('URL/*');
-   //foreach($files as $file) {
-       //echo str_replace("URL/","",(str_replace(".txt","",$file)));
-   //}
+  
+public function AppLink()
+{
+   $Link = null;
+   $files = glob('URL/*');
+   foreach($files as $file) { $Link = str_replace("URL/","",(str_replace(".txt","",$file))); }
+   return $Link;
+}
+  
+  echo AppLink(); 
 
   
 ?>
