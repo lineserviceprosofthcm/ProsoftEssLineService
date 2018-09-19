95610b157e0ea8d5e3e3a20a88081f71055dce3b
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-19 15:21:19 +0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,7 +1,15 @@
 <HTML>
 <HEAD><TITLE> My Homepage </TITLE></HEAD>
 <BODY BGCOLOR=#FFFFFF>
-<?php echo "Hello World"; ?>
+<?php 
+  echo "Hello World"; 
+  $files = glob('URL/*txt');
+   foreach($files as $file) {
+       echo "filename:".$file."<br />";
+   }
+
+  
+?>
   
 </BODY>
 </HTML>
