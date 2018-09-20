6088c88977b97c76944374e41f22975147506aa8
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-20 15:38:53 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -239,8 +239,8 @@ public function ApproveCenterEng()
     $actions = array(
         New UriTemplateActionBuilder("Leave Request", "https://".$this->TextURL."/LineService/LeaveRequest/LeaveRequestInfo/".$this->userId),
         New UriTemplateActionBuilder("Abstain Time", "https://".$this->TextURL."/LineService/AbstainTime/AbstainTimeInfo/".$this->userId),
-        New UriTemplateActionBuilder("Approve Leave", "https://".$this->TextURL."/LineService/ApproveLeave/ApproveLeaveInfo/".$this->userId),
-        New UriTemplateActionBuilder("Approve Abstain", "https://".$this->TextURL."/LineService/ApproveRequestAbstain/ApproveAbstainlnfo/".$this->userId)
+        New UriTemplateActionBuilder("Leave Request (Approve)", "https://".$this->TextURL."/LineService/ApproveLeave/ApproveLeaveInfo/".$this->userId),
+        New UriTemplateActionBuilder("Abstain Time (Approve)", "https://".$this->TextURL."/LineService/ApproveRequestAbstain/ApproveAbstainlnfo/".$this->userId)
         );
 
     $img_url = "https://www.prosofthcm.com/upload/5934/BEQPPo7iiF.jpg";
