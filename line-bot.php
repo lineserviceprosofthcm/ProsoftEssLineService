1285f5f87818025f17de1a4203076ce47ab153b1
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-20 09:03:15 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -602,14 +602,5 @@ public function LocationMessage($replyToken = null, $text)
         ]);
     }
 }
-function AppLink()
-{
-   $Link = null;
-   $files = glob('URL/*');
-   foreach($files as $file) { $Link = str_replace("URL/","",(str_replace(".txt","",$file))); }
-   return $Link;
-}
-
-
 }
 ?>
