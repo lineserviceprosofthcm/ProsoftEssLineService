8e8c44c9f9154b6d13e4119f4db3a7b10ff62e15
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-20 14:28:23 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -626,45 +626,5 @@ public function LocationMessage($replyToken = null, $text)
         ]);
     }
 }
-
-public function pho123($replyToken = null)
-{
-    $imageMapUrl = "https://lineservice.prosofthcm.com/upload/Resource/img.png";
-    $base = new BaseSizeBuilder(699,1040);
-    $imgmap = array(
-        new ImagemapMessageActionBuilder("Test", new AreaBuilder(0,0,355,699)),
-        new ImagemapMessageActionBuilder("Test", new AreaBuilder(686,0,354,699))
-    );
-    $replyData = new ImagemapMessageBuilder($imageMapUrl,"Imgmap",$base,$imgmap);
-
-    $test1 = new LocationMessageBuilder("TESTTi","SSS","12.12","21.21");
-
-    $asd = new AreaBuilder(0,0,355,699);
-    $az = new ImagemapUriActionBuilder("https://lineservice.prosofthcm.com/upload/Resource/img.png",$asd);
-
-    $sti = new StickerMessageBuilder("1","2563");
-
-        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
-            'replyToken' => $replyToken,
-            'messages'   => $replyData->buildMessage(),
-        ]);
-}
-
-public function pho1234($replyToken = null)
-{
-
-    $temp = new UriTemplateActionBuilder("Uri","https://www.google.co.th");
-    $actions = array(
-        new ImageCarouselColumnTemplateBuilder("https://www.prosofthcm.com/upload/5934/5d1apZw0Oh.jpg",$temp),
-        new ImageCarouselColumnTemplateBuilder("https://www.prosofthcm.com/upload/5934/5d1apZw0Oh.jpg",$temp)
-    );
-    $button = new ImageCarouselTemplateBuilder($actions);
-    $outputText = new TemplateMessageBuilder("ImageCarousel", $button);
-    $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
-        'replyToken' => $replyToken,
-        'messages'   => $replyData->buildMessage(),
-    ]);
-}
-    
 }
 ?>
