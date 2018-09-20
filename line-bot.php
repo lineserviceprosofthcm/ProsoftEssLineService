d011ee61342c37b35324260721db3b854b0f6c56
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-20 09:37:21 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -63,6 +63,8 @@ class BOT_API extends LINEBot
     public $response        = null;
 
     public $userId          = null;
+    public $TextURL         = null;
+    $$TextURL               = AppLink();
 
     /* ====================================================================================
      * Custom
@@ -602,5 +604,13 @@ public function LocationMessage($replyToken = null, $text)
         ]);
     }
 }
+function AppLink()
+{
+   $Link = null;
+   $files = glob('URL/*');
+   foreach($files as $file) { $Link = str_replace("URL/","",(str_replace(".txt","",$file))); }
+   return $Link;
+}
+
 }
 ?>
