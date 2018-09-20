63d44663109e1d2d6b8243fd1984d2e436f39747
ProsoftHCM <40351740+ProsoftHCM@users.noreply.github.com>
2018-09-20 09:17:34 +0700

    Update Test.php

--- Test.php
+++ Test.php
@@ -4,15 +4,24 @@
 <?php 
   
 include('line-bot.php');
+  
+
 
 function show()
 {
    $Link = AppLink();
-   echo AppLink(); 
+   echo $Link; 
 }
   
  show();
-  
+ 
+function AppLink()
+{
+   $Link = null;
+   $files = glob('URL/*');
+   foreach($files as $file) { $Link = str_replace("URL/","",(str_replace(".txt","",$file))); }
+   return $Link;
+}
 
 ?>
   
