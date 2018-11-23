4ec3f61bcc9ceeb74909db5204c3aba98ab9e56c
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2018-11-23 15:26:20 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-app.all('*', function(req, res, next) {
-     var origin = req.get('origin'); 
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
-});
