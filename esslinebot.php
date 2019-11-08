72ed4410f63af694f41e906a2aa09e80dfe3220a
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-08 13:03:06 +0700

    Update esslinebot.php

--- esslinebot.php
+++ esslinebot.php
@@ -6,14 +6,14 @@ include('essconnect.php');
 $channelSecret = 'b777323834edf3fd96558faf97a3a69a';
 $access_token  = '5ZP5bMi9tgEXR9Zwq5+TBJ6C5pv5SMbaIWBes4l1MGHQph3JbHhQyqRWD+e7pxEKsmwy0i5qQWc7gEAhudIS2GuUPEQmNY21zhsL0nrRbkQVCVUl8HoKS7s11KpkPiOXIapgGt7EALkwBLicU19DqgdB04t89/1O/w1cDnyilFU=';
 
-$NewsHDID = $_POST['NewsHDID'];
-$News = $_POST['News'];
-$LineIDLeaveRecord = $_POST['LineIDLeaveRecord'];
-$Detail = $_POST['Detail'];
-$LineID_NextApprove = $_POST['LineID_NextApprove'];
-$WaitApprove = $_POST['WaitApprove'];
-$LineID_EmpID = $_POST['LineID_EmpID'];
-$ApproveStatus = $_POST['ApproveStatus'];
+$NewsHDID = $_GET['NewsHDID'];
+$News = $_GET['News'];
+$LineIDLeaveRecord = $_GET['LineIDLeaveRecord'];
+$Detail = $_GET['Detail'];
+$LineID_NextApprove = $_GET['LineID_NextApprove'];
+$WaitApprove = $_GET['WaitApprove'];
+$LineID_EmpID = $_GET['LineID_EmpID'];
+$ApproveStatus = $_GET['ApproveStatus'];
 $bot = new BOT_API($channelSecret, $access_token);
 
 
@@ -229,5 +229,6 @@ if ($bot->isSuccess())
 
 // Failed
 echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
+echo "test";
 exit();
 ?>
