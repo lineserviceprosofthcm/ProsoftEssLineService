68c1f2c9f7b6039608f5fbcfc0edb4fb7c0b8833
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-08 15:33:57 +0700

    Add files via upload

--- vvvvvv.php
+++ vvvvvv.php
@@ -7,5 +7,10 @@ if(!empty($NewsHDID)){
 	echo $_GET['News']."|".$News;
 }
 echo "asdf";
-$homepage = file_get_contents('https://responsive.prosoft.co.th/api/LanguageSettingAPI/U3b8800b03f1c9d49899b6fd2da70bbb6/th-TH');
+
+	$x = file_get_contents("https://responsive.prosoft.co.th/api/LanguageSettingAPI/U3b8800b03f1c9d49899b6fd2da70bbb6/th-TH");
+	echo $x;
+	echo "WOW";
+    $open = json_decode($x, true);
+	echo $open;
 ?>
