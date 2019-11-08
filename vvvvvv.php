d4eae7ec0ae76b77fddbeff50e1f2eea9ae27ccc
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-08 17:01:26 +0700

    Add files via upload

--- vvvvvv.php
+++ vvvvvv.php
@@ -1,5 +1,6 @@
 <?php 
 require('line-bot.php');
+include('essconnect.php');
 $channelSecret = 'b777323834edf3fd96558faf97a3a69a';
 $access_token  = '5ZP5bMi9tgEXR9Zwq5+TBJ6C5pv5SMbaIWBes4l1MGHQph3JbHhQyqRWD+e7pxEKsmwy0i5qQWc7gEAhudIS2GuUPEQmNY21zhsL0nrRbkQVCVUl8HoKS7s11KpkPiOXIapgGt7EALkwBLicU19DqgdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
