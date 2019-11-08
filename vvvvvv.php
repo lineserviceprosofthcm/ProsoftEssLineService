043f12843d7ebfb2bab7d5e0e2f9de975ad4ba24
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-08 17:14:30 +0700

    Add files via upload

--- vvvvvv.php
+++ vvvvvv.php
@@ -28,7 +28,190 @@ if(!empty($va)){
 }
 
 if (!empty($bot->isEvents)) {
-	$bot->SendMessageTo("U3b8800b03f1c9d49899b6fd2da70bbb6","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-	echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    $Language = GetLanguage($bot->userId);
+    if($Language == "th-TH")
+    {
+        switch($bot->text){
+            case "Approve Center":
+                $bot->ApproveCenter();
+            break;
+            case "Time Attendance":
+                $bot->TimeAttendance();
+            break;
+            case "สิทธิ์การลา/วันลาคงเหลือ":
+                $Text = LeaveRemainNum($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "Payroll":
+                $bot->Payroll();
+            break;
+            case "ขอสลิปเงินเดือน":
+                $Text = EPaySlip($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "ขอเอกสาร 50 ทวิ":
+                $Text = Withholdingtaxcertificate($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "ขอใบรับรองการทำงาน":
+                $Text = WorkCert($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "ขอเอกสารรับรองเงินเดือน":
+                $Text = SalaryCert($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "Organization":
+                $bot->Organization();
+            break;
+            case "วันหยุดองค์กร":
+                $Text = calendar($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "ที่ตั้งองค์กร":
+                $Text = LocationOrganization($bot->userId);
+                if($Text == "ชื่อผู้ใช้ของคุณ ยังไม่ได้ลงทะเบียน" || $Text == "Please register to use system." || $Text == "ไม่พบที่อยู่องค์กร" || $Text == "not find Locationtion of Organization."){
+                    $bot->replyMessageNew($bot->replyToken,$Text);
+                }else{
+                    $bot->LocationOrg($bot->replyToken,$Text);
+                }
+            break;
+            case "Setting":
+                $bot->Setting();
+            break;
+            case "เปลี่ยนภาษา":
+                $bot->SendLanguage($bot->replyToken,$bot->userId);
+            break;
+            case "ภาษาไทย (Thai)":
+                $Text = ChangeLanguage($bot->userId,$bot->text);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "ภาษาอังกฤษ (English)":
+                $Text = ChangeLanguage($bot->userId,$bot->text);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            default:
+                $bot->BOT_New($bot->replyToken,$bot->text);
+            break;
+        }
+    }
+    else if($Language == "en-US") //#####################################################################################
+    {
+        switch($bot->text){
+            case "Approve Center":
+                $bot->ApproveCenterEng();
+            break;
+            case "Time Attendance":
+                $bot->TimeAttendanceEng();
+            break;
+            case "Leave Remain":
+                $Text = LeaveRemainNumEng($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "Payroll":
+                $bot->PayrollEng();
+            break;
+            case "E-Pay Slip":
+                $Text = EPaySlip($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "50 Bis Request":
+                $Text = Withholdingtaxcertificate($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "Works Cer. Request":
+                $Text = WorkCert($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "Salary Cer. Request":
+                $Text = SalaryCert($bot->userId);
+                if(count($Text) > 1){
+                    $bot->SendMessageTo($Text[1],$Text[0]); // ส่งข้อความหาHR
+                    $bot->replyMessageNew($bot->replyToken,$Text[2]); // ส่งข้อความหาผู้ขอ
+                }else{
+                    $bot->replyMessageNew($bot->replyToken,$Text[0]); // ส่งข้อความหาผู้ขอ
+                }
+            break;
+            case "Organization":
+                $bot->OrganizationEng();
+            break;
+            case "Organization Calendar":
+                $Text = CalendarEng($bot->userId);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "Location of Organization":
+                $Text = LocationOrganization($bot->userId);
+                if($Text == "ชื่อผู้ใช้ของคุณ ยังไม่ได้ลงทะเบียน" || $Text == "Please register to use system." || $Text == "ไม่พบที่อยู่องค์กร" || $Text == "not find Locationtion of Organization."){
+                    $bot->replyMessageNew($bot->replyToken,$Text);
+                }else{
+                    $bot->LocationOrg($bot->replyToken,$Text);
+                }
+            break;
+            case "Setting":
+                $bot->SettingEng();
+            break;
+            case "Language":
+                $bot->SendLanguage($bot->replyToken,$bot->userId);
+            break;
+            case "ภาษาไทย (Thai)":
+                $Text = ChangeLanguage($bot->userId,$bot->text);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            case "ภาษาอังกฤษ (English)":
+                $Text = ChangeLanguage($bot->userId,$bot->text);
+                $bot->replyMessageNew($bot->replyToken,$Text);
+            break;
+            default:
+                $bot->BOT_New($bot->replyToken,$bot->text);
+            break;
+        }
+    }
+    else if($Language == "NoLang")
+    {
+        if($bot->text == "ภาษาไทย (Thai)" || $bot->text == "ภาษาอังกฤษ (English)"){
+            $Text = ChangeLanguage($bot->userId,$bot->text);
+            $bot->replyMessageNew($bot->replyToken,$Text);
+        }else{
+            $bot->SendLanguage($bot->replyToken,$bot->userId);
+        }
+    }
+    else
+    { // Check Connect DB
+        $bot->replyMessageNew($bot->replyToken,"ยังไม่ได้เชื่อมต่อกับฐานข้อมูล\nNot connection DB.");
+    }
 }
+
+if ($bot->isSuccess()) 
+{
+  echo 'Succeeded!';
+  exit();
+}
+
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
+exit();
 ?>
