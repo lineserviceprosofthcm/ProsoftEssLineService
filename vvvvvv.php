1c4d190fda4f96b384182d2ba23bba024b6486e3
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-08 15:40:40 +0700

    Add files via upload

--- vvvvvv.php
+++ vvvvvv.php
@@ -1,16 +1,116 @@
-<?php
-$NewsHDID = $_GET['NewsHDID'];
-$News = $_GET['News'];
-if(!empty($NewsHDID)){
-	echo "ok";
-	echo $_GET['NewsHDID']."|".$NewsHDID;
-	echo $_GET['News']."|".$News;
-}
-echo "asdf";
+<?php 
+include('vendor/autoload.php');
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+//use LINE\LINEBot\Event;
+//use LINE\LINEBot\Event\BaseEvent;
+//use LINE\LINEBot\Event\MessageEvent;
+use \LINE\LINEBot\MessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use \LINE\LINEBot\ImagemapActionBuilder;
+use \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\TemplateActionBuilder;
+use \LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 
-	$x = file_get_contents("https://responsive.prosoft.co.th/api/LanguageSettingAPI/U3b8800b03f1c9d49899b6fd2da70bbb6/th-TH");
-	echo $x;
-	echo "WOW";
-    $open = json_decode($x, true);
-	echo $open;
+/*echo file_get_contents("URL/LineAPI.txt");*/
+
+class BOT_API extends LINEBot
+{
+/* ====================================================================================
+     * Variable
+     * ==================================================================================== */
+
+    private $httpClient     = null;
+    private $endpointBase   = null;
+    private $channelSecret  = null;
+
+    public $content         = null;
+    public $events          = null;
+
+    public $isEvents        = false;
+    public $isText          = false;
+    public $isImage         = false;
+    public $isSticker       = false;
+    public $isImagemap      = false;
+    public $isMessage       = false;
+
+    public $text            = null;
+    public $replyToken      = null;
+    public $source          = null;
+    public $message         = null;
+    public $timestamp       = null;
+
+    public $response        = null;
+
+    public $userId          = null;
+    public $TextURL         = null;
+
+    /* ====================================================================================
+     * Custom
+     * ==================================================================================== */
+    const DEFAULT_ENDPOINT_BASE = 'https://api.line.me';
+
+    public function __construct($channelSecret, $access_token)
+    {
+        $this->httpClient     = new CurlHTTPClient($access_token);
+        $this->channelSecret  = $channelSecret;
+        $this->endpointBase   = LINEBot::DEFAULT_ENDPOINT_BASE;
+
+        $this->content        = file_get_contents('php://input');
+        $events               = json_decode($this->content, true);
+
+        if (!empty($events['events'])) {
+            $this->isEvents = true;
+            $this->events   = $events['events'];
+
+            foreach ($events['events'] as $event) {
+                $this->replyToken = $event['replyToken'];
+                $this->source     = (object) $event['source'];
+                $this->message    = (object) $event['message'];
+                $this->timestamp  = $event['timestamp'];
+                $this->userId     = $event['source']['userId'];
+                
+                $files = glob('URL/*');
+                foreach($files as $file) { 
+					$this->TextURL    = str_replace("_","/",(str_replace("URL/","",(str_replace(".txt","",$file)))));
+				}
+                
+                /*$this->TextURL    = file_get_contents("URL/LineAPI.txt");*/
+                /*$this->TextURL    = "prosoft.gotdns.com/ESS/";*/
+                
+                    
+                if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+                    $this->isText = true;
+                    $this->text   = $event['message']['text'];
+                }
+
+            }
+        }
+
+        parent::__construct($this->httpClient, [ 'channelSecret' => $channelSecret ]);
+    }
+}
+echo $files = glob('URL/*');
 ?>
