c0eda85f459897267b25405487561588bc474cf2
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-11 11:04:09 +0700

    Add files via upload

--- vvvvvv.php
+++ vvvvvv.php
@@ -40,18 +40,47 @@ if(!empty($va)){
     /    'message' => 'An error occured...'
     /);
     //echo json_encode($response);
-	$myXMLData =
-"<?xml version='1.0' encoding='UTF-8'?>
-<note>
-<to>Tove</to>
-<from>Jani</from>
-<heading>Reminder</heading>
-<body>Don't forget me this weekend!</body>
-</note>";
+	$dom = new DOMDocument();
 
-$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-print_r($xml);
-}
+		$dom->encoding = 'utf-8';
+
+		$dom->xmlVersion = '1.0';
+
+		$dom->formatOutput = true;
+
+	$xml_file_name = 'movies_list.xml';
+
+		$root = $dom->createElement('Movies');
+
+		$movie_node = $dom->createElement('movie');
+
+		$attr_movie_id = new DOMAttr('movie_id', '5467');
+
+		$movie_node->setAttributeNode($attr_movie_id);
+
+	$child_node_title = $dom->createElement('Title', 'The Campaign');
+
+		$movie_node->appendChild($child_node_title);
+
+		$child_node_year = $dom->createElement('Year', 2012);
+
+		$movie_node->appendChild($child_node_year);
+
+	$child_node_genre = $dom->createElement('Genre', 'The Campaign');
+
+		$movie_node->appendChild($child_node_genre);
+
+		$child_node_ratings = $dom->createElement('Ratings', 6.2);
+
+		$movie_node->appendChild($child_node_ratings);
+
+		$root->appendChild($movie_node);
+
+		$dom->appendChild($root);
+
+	$dom->save($xml_file_name);
+
+	echo "$xml_file_name has been successfully created";
 
 if (!empty($bot->isEvents)) {
     $Language = GetLanguage($bot->userId);
