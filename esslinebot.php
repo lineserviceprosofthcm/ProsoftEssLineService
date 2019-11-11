7708f43a63efeb29abd1d2a5f98b255cda8fddb2
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-11 11:37:46 +0700

    Update esslinebot.php

--- esslinebot.php
+++ esslinebot.php
@@ -25,7 +25,8 @@ if(!empty($NewsHDID)){
         $bot->SendMessageTo($arr[$i],$News);
     }
     
-    return json_encode(array("result"=>true));
+    //return json_encode(array("result"=>true));
+    return true;
 }
 
 // แจ้งเอกสารลาหาผู้อนุมัติ
