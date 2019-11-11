17fc0bb6a06340c5bf4b82808aa92ac26a7c2951
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-11 10:18:12 +0700

    Update esslinebot.php

--- esslinebot.php
+++ esslinebot.php
@@ -34,7 +34,7 @@ if(!empty($NewsHDID)){
     
     $myXMLData = "<?xml version='1.0' encoding='UTF-8'?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
     $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-    echo $xml;
+    print_r($xml);
 }
 
 // แจ้งเอกสารลาหาผู้อนุมัติ
