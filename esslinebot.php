6fe82f2d49596674a4312044cfc96b8a7bdca607
ProsoftHCM <44012628+lineserviceprosofthcm@users.noreply.github.com>
2019-11-11 10:16:16 +0700

    Update esslinebot.php

--- esslinebot.php
+++ esslinebot.php
@@ -24,13 +24,17 @@ if(!empty($NewsHDID)){
     //for ($i = 0; $i<$iCount; $i++) {
         //$bot->SendMessageTo($arr[$i],$News);
     //}
-    $response = array(
-        status => true,
-        message => $NewsHDID,
-        message2 => $News,
-        message3 => $arr
-    );
+    //$response = array(
+        //status => true,
+        //message => $NewsHDID,
+        //message2 => $News,
+        //message3 => $arr
+    //);
     echo json_encode($response);
+    
+    $myXMLData = "<?xml version='1.0' encoding='UTF-8'?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+    $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
+    echo $xml;
 }
 
 // แจ้งเอกสารลาหาผู้อนุมัติ
